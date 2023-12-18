4688552c2d8dd49e58592fd688c2e47911a2e097
MV <m.viey@wanadoo.fr>
2023-12-18 16:51:55 +0100

    [test] fuzz testing

--- test/SimpleStorage.t.sol
+++ test/SimpleStorage.t.sol
@@ -1,14 +1,21 @@
-pragma solidity 0.8.10;
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
 
 import "forge-std/Test.sol";
-import "../contracts/SimpleStorage.sol";
+import "../src/SimpleStorage.sol";
  
-contract TestSimpleStorage {
+contract TestSimpleStorage is Test {
+    address user1 = makeAddr("user1");
+    address user2 = makeAddr("user2");
+    SimpleStorage simpleStorage;
  
- function testItStoresAValue() public {
-   SimpleStorage simpleStorage = SimpleStorage(DeployedAddresses.SimpleStorage());
-   simpleStorage.set(89);
-   uint expected = 89;
-   Assert.equal(simpleStorage.get(), expected, "It should store the value 89.");
- }
+    function setUp() public {
+        simpleStorage = new SimpleStorage();
+    }
+    
+    function testItStoresAValue(uint256 number) public {
+        simpleStorage.set(number);
+        uint expected = number;
+        assertEq(simpleStorage.get(), expected);
+    }
 }
